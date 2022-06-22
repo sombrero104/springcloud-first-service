ce0b289bcfd8eb753169e87731df1a2cd38c4fef
sombrero104 <sombrero.khy@gmail.com>
2022-06-22 21:07:52 +0900

    .

--- src/main/java/me/FirstServiceController.java
+++ src/main/java/me/FirstServiceController.java
@@ -1,6 +1,7 @@
 package me;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,12 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        System.out.println(header);
+        return "Hello World in First Service.";
     }
 
 }
