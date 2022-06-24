cc053e5406c79fa38bf2636703afdfb13381d39e
sombrero104 <sombrero.khy@gmail.com>
2022-06-24 22:54:11 +0900

    .

--- src/main/java/me/FirstServiceController.java
+++ src/main/java/me/FirstServiceController.java
@@ -1,15 +1,28 @@
 package me;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 // @RequestMapping("/")
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
+
+    Environment environment;
+
+    @Autowired
+    public FirstServiceController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,8 +36,12 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format(
+                "Hi, there. This is a message from First Service on PORT %s"
+                , environment.getProperty("local.server.port")); // 랜덤 포트 정보를 가져온다.
     }
 
 }
